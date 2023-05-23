5543aff7cc6a5173d8db2a18fdecae41c9e226a0
somnidev <36505144+somnidev@users.noreply.github.com>
2023-05-23 14:28:57 +0200

    Get the catfacts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Catfacts
+
+Grabs some Catfacts from the web.
+Based upon [Introduction to Golang - Online Workshop (w/ Le Wagon Tokyo) by Sam Hennessy](https://www.youtube.com/watch?v=mhoc3m49muk)
--- catfacts.go
+++ catfacts.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Catfact struct {
+	Fact   string
+	Length int
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	// https://catfact.ninja/fact
+	resp, err := http.Get("https://catfact.ninja/fact")
+	if err != nil {
+		fmt.Println("Error Get: ", err)
+		return
+	}
+	// fmt.Println(resp)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error Read: ", err)
+		return
+	}
+
+	var fact Catfact
+	json.Unmarshal(body, &fact)
+	//fmt.Printf("%#v", string(body))
+	fmt.Println(fact.Fact)
 }
